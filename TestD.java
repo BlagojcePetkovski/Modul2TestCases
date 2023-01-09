842736c2077d9c8341b53266d0a552ed991701cc
BlagojcePetkovski <blagojcepetkovski@yahoo.com>
2023-01-09 01:13:12 +0100

    add TestD

---
+++ TestD.java
@@ -0,0 +1,63 @@
+package testData;
+
+public class TestD {
+
+	//Users
+		public String validUserName = "blagojce@hotmail.com";
+		public String validPassword = "Blagojce1";
+		public String inValidUserName = "blagojcep@hotmail.com";
+		public String inValidPassword = "Blagojce123";
+		public String firstname = "Blagojce";
+		public String lastname = "Petkovski";
+		public String email = "blagojce12@hotmail.com";
+		public String password = "Blagojce1";
+		public String confirmpassword = "Blagojce1";
+		public String invalidconfirmpassword = "Blagojce12";
+		public String cpuType = "IntelCorei5";
+		public String memory = "8Gb";
+		public String friendsEmail = "nikola1@yahoo.com";
+		public String yourEmailaddress = "blagojce2@hotmail.com";
+		public String quantityField = "5";
+		public String Size3X = "3x";
+		public String ProductQuantityField = "4";
+		public String Sku = "NK_TLS_RS";
+		public String SkuInShoppingCart = "NK_TLS_RS";
+		public String QuantityField = "3";
+		public String QuantityField1 = "7";
+		public String QuantityField2 = "10";
+		public String PriceForOnePiece = "$43.50";
+		public String PriceForThreePieces = "$40.00";
+		public String PriceForSevenPieces = "$38.00";
+		public String PriceForTenPieces = "$35.00";
+		public String FirstNameCheckOut = "Ivan";
+		public String LastNameCheckOut = "Ivanov";
+		public String EmailCheckOut = "ivanovivan@email.com";
+		public String CityCheckOut = "Moscow";
+		public String Address1CheckOut = "Leninsky Ave 20";
+		public String ZipCodeCheckOut = "101000";
+		public String PhoneNumberCheckOut = "787-0000";
+		public String SelectCreditCardCheckOut = "Master card";
+		public String CardHolderNameCheckOut = "Ivan Ivanov";
+		public String CardNumberCheckOut = "5159661410754896";
+		public String CardCodeCheckOut = "164";
+
+		
+		//Titles
+		public String homePageTitle = "nopCommerce demo store";
+		public String loginPageTitle = "nopCommerce demo store. Login";
+		public String registerPagetitle ="nopCommerce demo store. Register";
+		public String plpPageTitle = "https://demo.nopcommerce.com/hp-envy-6-1180ca-156-inch-sleekbook";
+		public String plpPageTitle1 = "https://demo.nopcommerce.com/notebooks?viewmode=grid&orderby=0&pagesize=6&specs=6%2C9";
+		public String pdpMailaFriend = "https://demo.nopcommerce.com/productemailafriend/39";
+		public String wishListTitle = "https://demo.nopcommerce.com/cart";
+		public String wishListTotalPrice = "https://demo.nopcommerce.com/wishlist";
+		public String aboutUsButoon = "https://demo.nopcommerce.com/about-us";
+		public String updatedCart = "https://demo.nopcommerce.com/cart";
+		public String updatedCart1 = "https://demo.nopcommerce.com/cart";
+		public String updatedCart2 = "https://demo.nopcommerce.com/cart";
+		public String checkoutPage = "https://demo.nopcommerce.com/login/checkoutasguest?returnUrl=%2Fcart";
+		public String checkoutPage1 = "https://demo.nopcommerce.com/";
+
+	}
+
+
